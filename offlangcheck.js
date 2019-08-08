b05a8280ddb3b3abaafa12732a0fff2736901e89
LordEggNutz <jeremycar121@gmail.com>
2019-08-08 12:25:19 -0700

    Create offlangcheck.js

---
+++ offlangcheck.js
@@ -0,0 +1,31 @@
+/*************************************************************************
+ * Project Name: Offensive Language Check
+ * Version: 1.1
+ * Date: August 8, 2019
+ * Author: LEN Design Concepts 
+ * Description: This script is for checking name generators results against a list
+ * of names that would be considered offensive.
+ * Call this script with checkLanguage(randomName);
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+****************************************************************************/
+
+var checkLanguage = function(swear){
+  var good = swear.toLowerCase();
+  if(lang.indexOf(name) === -1){
+    return good;
+  }else{
+    randomName = "";
+  }
+
+var lang = ["alah","allah","anal","anilingus","anus","apeshit","arse","arsehole","ass","asshole","assmunch","autoerotic","babeland","balls","ballsack","bangbros","bareback","barenaked","bastard","bastardo","bastinado","beaner","beaners","bestiality","biatch","bigtits","bimbos","birdlock","bitch","bitches","black","bloody","blowjob","blumpkin","bollock","bollocks","bollok","bondage","boner","boob","boobs","bugger","bukkake","bulldyke","bullshit","bum","bunghole","busty","butt","buttcheeks","butthole","buttplug","cameltoe","camgirl","camslut","camwhore","clit","clitbeard","clitoris","cloaka","clusterfuck","cock","cocks","coon","coons","cornhole","crap","creampie","cum","cumming","cunt","damn","darkie","daterape","deepthroat","dick","dildo","doggy","dolcett","domination","dominatrix","dommes","dryhump","dyke","ecchi","ejaculation","erotic","erotism","escort","eunuch","fag","fagget","faggit","faggot","faggut","faghet","faghit","faghot","faghut","fecal","feck","felch","felching","fellate","fellatio","feltch","femdom","fetish","figging","fingerbang","fingering","fisting","flange","footjob","frotting","fuck","fuckin","fucking","fucktard","fucktards","fudgepacker","futanari","gangbang","gaysex","genitals","goatcx","goatse","god","goddamn","gokkun","goodpoop","googirl","goregasm","grope","groupsex","guro","handjob","hardcore","hell","hentai","homo","homoerotic","honkey","hooker","humping","incest","intercourse","jackoff","jailbait","jerk","jerkoff","jigaboo","jiggaboo","jiggerboo","jizz","juggs","kike","kinbaku","kinkster","kinky","knobbing","knobend","kum","labia","lmao","lmfao","lolita","masturbate","milf","muff","nambla","nawashi","neeger","neegger","negger","negro","neonazi","nieger","niegger","niga","nigar","niger","nigga","niggar","niggas","niggaz","nigger","nigges","niggir","niggis","niggor","niggos","niggur","niggus","nigher","nighes","nignog","nigra","nimphomania","nipple","nipples","nude","nudity","nympho","nymphomania","obama","octopussy","omg","omorashi","oral","orgasm","orgy","paedo","paki","panties","panty","pedo","pegging","penis","pis","piss","pissing","pisspig","playboy","ponyplay","poof","poon","poontang","poop","porn","porno","prick","pube","pubes","punany","pussy","queaf","queef","queer","quim","raghead","rape","raping","rapist","rectum","rimjob","rimming","sadism","santorum","scat","schlong","scissoring","scrotum","semen","sex","sexo","sexy","shaved","shemale","shibari","shit","shitblimp","shitty","shota","shrimping","skeet","slanteye","slut","smegma","smut","snatch","sodomize","sodomy","spic","splooge","spooge","spunk","strapon","suck","sucks","suicide","sultry","swastika","swinger","threesome","throating","tiits","tit","tits","titties","titty","topless","tosser","towelhead","trani","tranie","tranni","trannie","tranny","trany","trennie","tubgirl","turd","tushy","twat","twink","twinkie","upskirt","urethra","urophilia","vagina","vibrator","voyeur","vulva","wank","wetback","whore","wtf","yaoi","yiffy"];
+};
